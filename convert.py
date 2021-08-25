3e4044780580c7b936a794c200d45e166bca1cce
Brian Loveless <Brian.loveless1@yahoo.com>
2021-08-25 14:35:09 -0400

    start csv is blank mock data in folder

---
+++ convert.py
@@ -0,0 +1,18 @@
+import json
+import csv
+
+with open('mock_data_10.json') as json_file:
+    jsondata = json.load(json_file)
+
+data_file = open('data_output.csv', 'w', newline='')
+csv_writer = csv.writer(data_file)
+
+count = 0
+for data in jsondata:
+    if count == 0:
+        header = data.keys()
+        csv_writer.writerow(header)
+        count += 1
+    csv_writer.writerow(data.values())
+
+data_file.close()
